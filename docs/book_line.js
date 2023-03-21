962e64afd0115978020a4a5e6f65d51b8ab7059e
Michele Tobias <mmtobias@ucdavis.edu>
2023-03-21 11:25:39 -0700

    add next point

--- docs/book_line.js
+++ docs/book_line.js
@@ -1,1 +1,1 @@
-var book_line={"type":"FeatureCollection","features":[{"type":"Feature","properties":{"V1":"book travel path"},"geometry":{"type":"LineString","coordinates":[[-104.984862,39.7392364],[-85.6678639,42.9632405]]}}]}
+var book_line={"type":"FeatureCollection","features":[{"type":"Feature","properties":{"V1":"book travel path"},"geometry":{"type":"LineString","coordinates":[[-104.984862,39.7392364],[-85.6678639,42.9632405],[-84.5553805,42.7337712]]}}]}
